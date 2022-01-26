05ff23d330e047fee549227a7750480efb61e83f
vikasallmighty <vikas12october@gmail.com>
2022-01-26 19:12:03 +0530

    requirement file and setup file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Covid-india-tracker-django-python-master',
+    version='1',
+    packages=['covidindia', 'covidtracker', 'covidtracker.migrations'],
+    url='https://github.com/vikasallmighty/CovidTracker',
+    license='mit',
+    author='vs808',
+    author_email='vikas12october@gmail.com',
+    description='covid tracker'
+)
