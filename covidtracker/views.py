ef634123b8034ee1906da4b13ba37fa13a94e32a
vikasallmighty <vikas12october@gmail.com>
2022-01-26 18:55:08 +0530

    api changes : added new api

--- covidtracker/views.py
+++ covidtracker/views.py
@@ -6,50 +6,39 @@ now = datetime.now()
 
 def district():
     api="https://api.covid19india.org/v2/state_district_wise.json"
-
+    api2 = "https://api.covid19tracker.in/data/static/data.min.json"
     s=(requests.get(api)).text
-
+    s2=(requests.get(api2)).text
     data=json.loads(s)
-    return data
-
-def stateWise(request):
-    api = "https://api.covid19india.org/v4/data.json"
-    s = (requests.get(api)).text
-    data = json.loads(s)
+    data2=json.loads(s2)
     statelist = ["Andaman and Nicobar Islands", "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chandigarh",
                  "Chattisgarh",
-                  "Delhi", "Dadar and Nagar Havelli", "Goa", "Gujarat", "Haryana", "Himachal Pradesh",
-                 "Jammu and Kashmir", "jharkhand", "Karnataka", "Kerala", "Ladakh",  "Madhya Pradesh",
+                 "Delhi", "Dadar and Nagar Havelli", "Goa", "Gujarat", "Haryana", "Himachal Pradesh",
+                 "Jammu and Kashmir", "jharkhand", "Karnataka", "Kerala", "Ladakh", "Madhya Pradesh",
                  "Maharashtra",
                  "Manipur", "Meghalya", "Mizoram", "Nagaland", "Orissa", "Punjab", "Pondicherry", "Rajastha", "Sikkim",
                  "Tamil Nadu",
-                 "Telangana", "Tripura", "Uttarakhand", "Uttar Pradesh", "West Bengal","Total"]
-    statecode = ["an", "ap", "ar", "as", "br", "ch", "ct",  "dl", "dn", "ga", "gj", "hr", "hp", "jk", "jh", "ka",
+                 "Telangana", "Tripura", "Uttarakhand", "Uttar Pradesh", "West Bengal", "Total"]
+    statecode = ["an", "ap", "ar", "as", "br", "ch", "ct", "dl", "dn", "ga", "gj", "hr", "hp", "jk", "jh", "ka",
                  "kl", "la",
                  "mp", "mh", "mn", "ml", "mz", "nl", "or", "pb", "py", "rj", "sk", "tn", "tg", "tr", "ut", "up", "wb"
-                ,"tt"]
-
-    lst = []
+        , "tt"]
+    dict = {}
     for i in range(len(statecode)):
-        dict = {}
-        l = data[statecode[i].upper()]
-        dict["state"] = statelist[i]
-        if "delta" in l.keys():
-            dict["daily"] = l["delta"]
-        else:
-            dict["daily"] = {"confirmed": 0,
-          "deceased": 0,
-          "recovered": 0,
-          "tested": 0}
-        dict["total"] = l["total"]
-        lst.append(dict)
+
+        l = data2[statecode[i].upper()]
+        dict[statelist[i]] = l
+    return dict
+def stateWise(request):
+    dict = district()
+    vaccinated = dict['Total']['total']['vaccinated1'] +  dict['Total']['total']['vaccinated2']
+    date = dict['Total']['meta']['last_updated']
     params = {
-        "ss": lst,
-        "ind": lst[-1],
-        "date": data["TT"]["meta"]["tested"]["last_updated"]
+        "ss": district(),
+        "v": vaccinated,
+        "date":date
     }
     return render(request, 'state.html', params)
-
 
 
 def DistrictWise(request):
